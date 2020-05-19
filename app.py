bcd1f14b3ccc60afb4021ccd12b175a91046e3ad
gvnbleid <krawczykmf@gmail.com>
2020-05-19 21:32:07 +0200

    Start implementing scroll

--- app.py
+++ app.py
@@ -4,14 +4,37 @@ import dash_html_components as html
 from dash import dependencies
 
 external_stylesheets = ['https://raw.githubusercontent.com/plotly/dash-app-stylesheets/master/dash-analytics-report.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-exploration_div = html.Div(id='exploration_div', hidden=False)
-model_div = html.Div(id='model_div', hidden=True)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_scripts=['/assets/navbar.js'])
+exploration_div = html.Div(id='exploration_div')
+model_div = html.Div(id='model_div')
 app.layout = html.Div([
-    html.Button('exploration', id='exploration_button', n_clicks=0),
-    html.Button('model', id='model_button', n_clicks=0),
-    exploration_div,
-    model_div
+    html.Div(
+        style={'overflow': 'hidden', 'width': '8%'},
+        id='navbar',
+        children=[
+            html.Ol([
+                html.Li(children=[html.A(href='#data-exploration', children=["Eksploracja danych"])]),
+                html.Li(children=[html.A(href='#model-features', children=["Właściwości modelu"])]),
+            ])
+        ]
+    ),
+    html.Div(style={'float': 'left', 'width': '8%', 'height': 1000}),
+    html.Div(style={
+        'border-left': '6px solid green',
+        'height': '2000px',
+        'float': 'left'
+    }),
+    html.Div(style={'float': 'left', 'width': '90%'},
+             children=[
+                 html.H2("Eksploracja danych", id='data-exploration'),
+                 exploration_div,
+                 html.H2("Właściwości modelu", id='model-features', style={'float': 'left', 'width': '100%'}),
+                 model_div
+             ])
+    # html.Button('exploration', id='exploration_button', n_clicks=0),
+    # html.Button('model', id='model_button', n_clicks=0),
+    # exploration_div,
+    # model_div
 ])
 
 
